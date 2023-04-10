d1178f931b5afbde7236ab9da742e32045872d95
sunil parmar <sunil04ce@gmail.com>
2023-04-10 23:44:53 +0530

    typescript types, type inference

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("TS code goes here..");
+var add = function (n1, n2, showResult, phrase) {
+    //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //     throw new Error("Innocent input!");
+    //   }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+};
+var number1 = 4;
+var number2 = 2.4;
+var printResult = true;
+var resultPhrase = "Result is : ";
+var result = add(number1, number2, printResult, resultPhrase);
+// console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
 console.log("TS code goes here..");
+
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+  //   if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //     throw new Error("Innocent input!");
+  //   }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+};
+
+let number1: number;
+number1 = 4;
+const number2 = 2.4;
+const printResult = true;
+let resultPhrase = "Result is : ";
+const result = add(number1, number2, printResult, resultPhrase);
+
+// console.log(result);
