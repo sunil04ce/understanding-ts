3dd9260a22f30239dd35e470561ead998e39e9a1
sunil parmar <sunil04ce@gmail.com>
2023-04-14 09:45:13 +0530

    moved code to data-types folder, exclude node_modules in tsconfig.json

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-// "unknown" type
-var userInput;
-var userName;
-userInput = 5;
-userInput = "Jack";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-// userName = userInput;
-// 'never' type
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-    //   while(true) {}
-}
-generateError("An error occured!", 500);
-// const result = generateError("An error occured!", 500);
-// console.log(result);
+"use strict";
+console.log("app.js");
---
+++ data-types/union-aliases.js
@@ -0,0 +1,15 @@
+"use strict";
+const combine = (input1, input2, resultConversion) => {
+    let result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+};
+console.log(combine(50, 47, "as-number"));
+console.log(combine("50", "40", "as-number"));
+console.log(combine("Jack", "Sparro", "as-text"));
