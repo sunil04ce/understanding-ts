851ca8cfa1afeed030be021936ad76e44b15f053
sunil parmar <sunil04ce@gmail.com>
2023-04-24 23:28:01 +0530

    constraints, use of keyof constraint

--- dist/app.js
+++ dist/app.js
@@ -10,4 +10,20 @@ promise.then((data) => {
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-console.log(merge({ name: "Jack" }, { age: 37 }));
+const mergedObj = merge({ name: "Jack", hobbies: ["Sports"] }, { age: 37 });
+console.log("age -> " + mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking"]));
+function extractAndConvert(obj, key) {
+    return "Value :-> " + obj[key];
+}
+console.log(extractAndConvert({ name: "Jack" }, "name"));
--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,36 @@ promise.then((data) => {
   // data.split(' ')
 });
 
-function merge<T, U>(objA: T, objB: U) {
-  //return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
-  { name: "Jack", hobbies: ["Sports"] },
-  { age: 37 }
-);
-//console.log("age -> " + mergedObj.age);
+const mergedObj = merge({ name: "Jack", hobbies: ["Sports"] }, { age: 37 });
+console.log("age -> " + mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+
+  return [element, descriptionText];
+}
+
+// console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value :-> " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Jack" }, "name"));
