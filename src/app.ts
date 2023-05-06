aadb117024a90100299363038472d3241e7a41da
sunil parmar <sunil04ce@gmail.com>
2023-05-06 16:12:27 +0530

    use of libraries: axios, google map

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,44 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-import _ from "lodash";
-import { Product2 } from "./product.model";
-import "reflect-metadata";
-import { plainToClass } from "class-transformer";
-import { validate } from "class-validator";
+import axios from "axios";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-console.log(_.shuffle([1, 2, 3]));
+const GOOGLE_API_KEY = "";
 
-const products = [
-  { title: "A book", price: 10.99 },
-  { title: "A carpet", price: 29.99 },
-];
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
-const loadedProducts = plainToClass(Product2, products);
-for (const product of loadedProducts) {
-  console.log(product.getInformation());
+async function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Cound not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 8,
+        }
+      );
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-const newProd = new Product2("", -10);
-validate(newProd).then((errors) => {
-  if (errors.length > 0) {
-    console.log("VALIDATION ERRORS!");
-    console.log(errors);
-  } else {
-    console.log(newProd.getInformation());
-  }
-});
+form.addEventListener("submit", searchAddressHandler);
