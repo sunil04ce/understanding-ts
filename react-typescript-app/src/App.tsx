f610af0df78bf58b64bf75bcc0dd5c516dfd9891
sunil parmar <sunil04ce@gmail.com>
2023-05-06 20:35:59 +0530

    todo app using reactjs and typescript

---
+++ react-typescript-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (id: string) => {
+    setTodos((prevTools) => {
+      return prevTools.filter((todo) => todo.id !== id);
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
